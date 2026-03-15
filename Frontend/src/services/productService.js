2c049954c4daea8bc1152dab08c589a9f512a311
mvtistr <matisepu.unab@gmail.com>
2026-03-15 15:29:34 -0300

    conectando front y back con axios

--- Frontend/src/services/productService.js
+++ Frontend/src/services/productService.js
@@ -1,15 +1,34 @@
-import products from "@data/products.js";
+import API from './api.js';
 
-export const fetchProducts = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(products);
-    }, 300);
-  });
+export const getProducts = async () => {
+    const res = await API.get('/products');
+    return res.data;
 };
 
-// Con Backend
-// export const fetchProducts = async () => {
-//   const res = await fetch("http://localhost:3000/products");
-//   return res.json();
-// };
+export const getProductById = async (id) => {
+    const res = await API.get(`/products/${id}`);
+    return res.data;
+};
+
+export const createProduct = async (productData) => {
+    const res = await API.post('/products', productData);
+    return res.data;
+};
+
+export const updateProduct = async (id, productData) => {
+    const res = await API.put(`/products/${id}`, productData);
+    return res.data;
+};
+
+export const deleteProduct = async (id) => {
+    const res = await API.delete(`/products/${id}`);
+    return res.data;
+};
+
+export const productService = {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct
+};
