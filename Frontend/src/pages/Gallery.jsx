e17df9c9e99260d05f8e3ef0caab87dccae83aa7
mvtistr <matisepu.unab@gmail.com>
2026-03-20 14:39:51 -0300

    correccion services & useEffect

--- Frontend/src/pages/Gallery.jsx
+++ Frontend/src/pages/Gallery.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { Icons } from "@shared/icons.js";
 
-import { getProducts } from "@services/productService";
+import { getProducts } from "@services/product.service.js";
 
 import "@styles/menu.css";
 
