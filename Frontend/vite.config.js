374960f34009a4a62729933bb025d9089d23c8d9
mvtistr <matisepu.unab@gmail.com>
2026-03-23 12:44:11 -0300

    outdir on vite.config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist"
+  },
   plugins: [react()],
     resolve: {
     alias: {
@@ -18,4 +21,4 @@ export default defineConfig({
       '@shared': fileURLToPath(new URL('./src/shared', import.meta.url))
     },
   },
-})
+});
